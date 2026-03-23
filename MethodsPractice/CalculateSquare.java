9913e83f82243c73592da64b131e84014e73a155
Camycoder <camycoder12@gmail.com>
2026-03-23 14:04:10 -0300

    feat: create mathematical method for squaring integers

---
+++ MethodsPractice/CalculateSquare.java
@@ -0,0 +1,10 @@
+/*Recebe um número inteiro e retorna o seu quadrado (num * num). */
+
+public class CalculateSquare {
+    public int calculateSquare(int num){
+      int square = num * num;
+
+      return square;
+
+    }
+}
